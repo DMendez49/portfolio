d7feb39853c9a47ec5171d6e30e8d0aef5a57f0c
David Mendez <mendezdavid68@gmail.com>
2020-01-20 22:18:55 -0700

    update resume

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -7,36 +7,43 @@ const Resume = () => (
   <center>
     <br /><br />
     <Button>
-      <a href="https://docs.google.com/document/d/1CFYHfLnYrZMGTb0rjlSLrG6dsdAUkg_wSJd0tUtoaUQ/edit" target="_blank">
-        <img alt="build diagram" src="https://img.icons8.com/plasticine/2x/resume.png" height="150" width="140"></img></a>
+      <a href="https://docs.google.com/document/d/1CFYHfLnYrZMGTb0rjlSLrG6dsdAUkg_wSJd0tUtoaUQ/edit"
+        target="_blank">
+        <img
+          alt="build diagram"
+          src="https://img.icons8.com/plasticine/2x/resume.png"
+          height="150"
+          width="140">
+        </img>
+      </a>
     </Button>
     <br /> <br /><br /><br />
     <a href="https://docs.google.com/document/d/1CFYHfLnYrZMGTb0rjlSLrG6dsdAUkg_wSJd0tUtoaUQ/edit" target="_blank">
-    <Card>
-      <h3>
-        <br />
-        Select Icon to review Resume
+      <Card>
+        <h3>
+          <br />
+          Select Icon to review Resume
         <br /><br />
-      </h3>
-    </Card></a>
+        </h3>
+      </Card></a>
     <br /><br /><br />
     <a href="https://docs.google.com/document/d/1CFYHfLnYrZMGTb0rjlSLrG6dsdAUkg_wSJd0tUtoaUQ/edit" target="_blank">
-    <card>
-      <div class="backson">
-        <br /><br /><br />
-        <h2>Refrences</h2>
-        <hr />
-        <br />
-       <h3>
-         <h3>Stanley Egwuonwu</h3>
-         <h3>Assistant Professor</h3>
-         <h3>Computer Sciences and Information Systems</h3>
-         <h3>Office: BB111P</h3>
-         <h3>Office phone: 801-957-4054</h3>
-         <h3>Email: Stanley.egwuonwu@slcc.edu</h3>
-         </h3>
-      </div>
-    </card></a>
+      <card>
+        <div class="backson">
+          <br /><br /><br />
+          <h2>Refrences</h2>
+          <hr />
+          <br />
+          <h3>
+            <h3>Stanley Egwuonwu</h3>
+            <h3>Assistant Professor</h3>
+            <h3>Computer Sciences and Information Systems</h3>
+            <h3>Office: BB111P</h3>
+            <h3>Office phone: 801-957-4054</h3>
+            <h3>Email: Stanley.egwuonwu@slcc.edu</h3>
+          </h3>
+        </div>
+      </card></a>
   </center>
 );
 
